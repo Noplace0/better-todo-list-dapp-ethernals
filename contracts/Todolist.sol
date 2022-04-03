e924ecca0b50e6d7b9519c467e09213903e5e938
fidio <fidio@live.hk>
2022-04-03 21:01:43 +0800

    inital commit

---
+++ contracts/Todolist.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Todolist{
+
+    //own data type
+    struct Task{
+        string task_name;
+        bool is_done;
+    }
+    
+    mapping (address =>Task[]) private Users;
+    function addTask (string calldata _task) external{
+        Users[msg.sender].push(Task({
+            task_name: _task,
+            is_done: false
+        }));
+    }
+
+
+    function getTask (uint _taskIndex) external view returns (Task memory){
+        //particular task index
+        Task memory task = Users[msg.sender][_taskIndex];
+        return task;
+    }
+
+    function updateTaskStatus (uint _taskIndex, bool _status) external {
+        Users[msg.sender][_taskIndex].is_done = _status;
+    }
+    function updateallTaskStatus (bool _status) external {
+        uint totaltask = Users[msg.sender].length;
+        for(uint i = 0; i < totaltask; i++){
+            Users[msg.sender][i].is_done = _status;
+        }
+    }
+
+    function deleteTask (uint _taskIndex) external {
+        delete Users[msg.sender][_taskIndex];
+    }
+    function deleteallTask () external {
+    delete Users[msg.sender];
+    }
+
+
+    function getTaskCount() external view returns (uint){
+        return Users[msg.sender].length;
+    }
+
+    //returns task name
+    function getTaskname(uint _taskIndex) external view returns (string memory){
+        Task memory task = Users[msg.sender][_taskIndex];
+        return task.task_name;
+    }
+}
---
+++ frontend/js/config.js
@@ -0,0 +1,130 @@
+let contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_task",
+				"type": "string"
+			}
+		],
+		"name": "addTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deleteallTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "deleteTask",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bool",
+				"name": "_status",
+				"type": "bool"
+			}
+		],
+		"name": "updateallTaskStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskIndex",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "_status",
+				"type": "bool"
+			}
+		],
+		"name": "updateTaskStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "getTask",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "task_name",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "is_done",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct Todolist.Task",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTaskCount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_taskIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "getTaskname",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+let contractAddress = "0x314E5da57D74fB3351F94c6F203552c18E31BF86";
